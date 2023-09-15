f34546351a4c058b184b7b17505420ad8257f4ab
JohanneGottenborg <johanne.g.hansen@gmail.com>
2023-09-15 08:50:57 +0200

    Update sieve.py

--- src/sieve.py
+++ src/sieve.py
@@ -17,4 +17,28 @@ def sieve(n: int) -> list[int]:
 
     # FIXME: fill out this bit
 
+    # for n in candidates:
+    #     primes.append(candidates[0])
+    #     # print(n)
+    #     for m in candidates:
+    #         if m // n == 0:
+    #             candidates.remove(m)
+    #         # print(m)    
+    #     print(candidates)
+    
+    l = list([0]*n)
+    for i in l:
+        if len(candidates) == 0:
+            break
+        p = candidates[i]
+        primes.append(p)
+        candidates.remove(p)
+        for c in candidates:
+            if c % p == 0:
+                candidates.remove(c)
+        #print(candidates)
+
+    #print(primes)
     return primes
+
+sieve(15)
